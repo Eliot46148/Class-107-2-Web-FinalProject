93f8ce4a8e1ab0e98549a03df3669f21b0633bf5
joseph871107 <joseph1231231234@gmail.com>
2019-06-12 21:00:19 +0800

    登入

--- script.js
+++ script.js
@@ -18,3 +18,74 @@ function AdjustIframeHeightOnLoad(id) {
         }
     }
 }
+
+const userName = document.getElementById('userName');
+const emailAddr = document.getElementById('emailAddr');
+const psword = document.getElementById('psword');
+const btnLogin = document.getElementById('login');
+const btnSignup = document.getElementById('signup');
+const btnSignout = document.getElementById('signout');
+const status = document.getElementById("status");
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log(user.email, "login");
+        loginUser = firebase.auth().currentUser;
+        status.innerText = user.email + "登入成功";
+    } else {
+        console.log("login failed");
+        loginUser = null;
+        status.innerText = "未登入";
+    }
+});
+
+btnLogin.addEventListener('click', e => {
+    const email = emailAddr.value;
+    const pswd = psword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, pswd);
+    promise.catch(e => alert(e.message));
+})
+btnSignup.addEventListener('click', e => {
+    const name = userName.value;
+    const email = emailAddr.value;
+    const pswd = psword.value;
+    const auth = firebase.auth();
+
+    const publicData = {
+        email: auth.currentUser.email,
+        name: name
+    };
+    const promise = auth.createUserWithEmailAndPassword(email, pswd)
+        .then(function () {
+            var updates = {};
+            updates['/user_group/public_user_data/' + auth.currentUser.uid] = publicData;
+            updates['/user_group/private_user_data/' + auth.currentUser.uid] = {
+                password: pswd
+            };
+            firebase.database().ref().update(updates)
+                .then(function () {
+                    alert('註冊成功');
+                    console.log("Sign Up");
+                    window.location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        });
+    promise.catch(e => alert(e.message));
+})
+
+btnSignout.addEventListener('click', e => {
+    const auth = firebase.auth();
+    auth.signOut()
+        .then(function () {
+            alert("成功登出");
+            console.log("Sign out");
+            window.location.reload();
+        })
+        .catch(function (e) {
+            console.log(e.message);
+        });
+})
